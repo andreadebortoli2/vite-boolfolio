7390357bf518820a85c21ebf9b4d9da09c9ba04b
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-30 16:43:34 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <header>Header</header>
+  <main>
+    <Card />
+  </main>
+  <footer>Footer</footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+  <div>placeholder card component</div>
+</template>
+
+<style scoped></style>
