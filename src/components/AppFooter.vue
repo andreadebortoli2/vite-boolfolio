d2aaf1bacc2f3b90a525783c9b16b051b32b679d
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-04 12:53:50 +0200

    edit footer style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,44 +15,33 @@ export default {
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-3">
+                <div class="col">
                     <div class="logo">
-                        LOGO
+                        <span>Andrea</span> De Bortoli
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit praesentium molestiae laborum dicta
-                        commodi deleniti repellendus sapiente quaerat pariatur doloremque?</p>
-                </div>
-                <div class="col-2 quick-links">
-                    <h4>
-                        Quick links
-                    </h4>
-                    <ul>
-                        <li v-for="route in store.routes">
-                            <router-link :to="{ name: route.route_name }">{{ route.view_name }}</router-link>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <h4>
-                        Find me on
-                    </h4>
-                    <ul class="social-links">
-                        <li>
-                            <a href="#">
-                                <i class="fa-brands fa-linkedin fa-fw" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fa-brands fa-youtube fa-fw" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fa-brands fa-github fa-fw" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
+
+                    <div class="find-on">
+                        <h4>
+                            Find me on:
+                        </h4>
+                        <ul class="social-links">
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-linkedin fa-fw" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-youtube fa-fw" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-github fa-fw" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -61,45 +50,46 @@ export default {
 
 <style scoped>
 footer {
-    background-color: gray;
-    padding-top: 1rem;
-    color: whitesmoke;
-}
+    background-color: var(--primary-dark);
+    color: var(--lightest);
 
-.row {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.col-3 {
-    width: calc(100% / 12 * 3);
-
-    p {
-        padding: 1rem 0;
-    }
-}
-
-.col-2 {
-    width: calc(100% / 12 * 2);
-    text-align: center;
-
-
-    ul {
-        list-style: none;
-        padding-top: 1rem;
-
-        a {
-            color: whitesmoke;
-            text-decoration: none;
-        }
-    }
-
-    .social-links {
+    .row {
         display: flex;
-        justify-content: center;
-        padding-top: 1rem;
-        gap: 1rem;
+        justify-content: flex-end;
+        align-items: center;
+
+        .col {
+            display: flex;
+            align-items: baseline;
+
+            .logo {
+                padding-right: 2rem;
+                font-size: 1rem;
+
+                span {
+                    font-size: 2rem;
+                }
+            }
+
+            .find-on {
+                display: flex;
+                align-items: center;
+                justify-content: space-evenly;
+
+                .social-links {
+                    padding-left: 1rem;
+                    list-style: none;
+                    display: flex;
+                    justify-content: space-around;
+                    gap: 1rem;
+
+                    a {
+                        color: var(--lightest);
+                        text-decoration: none;
+                    }
+                }
+            }
+        }
     }
 }
 </style>
