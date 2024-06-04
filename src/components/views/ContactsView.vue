43e9a53cd3a3bac7ea3985d34c2af13847993916
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-04 23:34:56 +0200

    edit contact style

--- src/components/views/ContactsView.vue
+++ src/components/views/ContactsView.vue
@@ -7,9 +7,19 @@ export default {
 <template>
     <section class="container">
         <h1>Contact me</h1>
-        <form action="" method="post">
-            <div class="wrapper">
-                <div class="left">
+        <div class="row">
+            <div class="col contact">
+                <p>directly on
+                    <a href="#">
+                        <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+                    </a>
+                </p>
+                <h2>or <br> send me a message</h2>
+            </div>
+            <div class="col form">
+
+                <form action="" method="post">
+
                     <div class="name">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" name="name" id="name" aria-describedby="helpId"
@@ -20,18 +30,18 @@ export default {
                         <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
                             placeholder="abc@mail.com" />
                     </div>
-                </div>
-                <div class="right">
+
                     <div class="message">
                         <label for="message" class="form-label">Message</label>
                         <textarea class="form-control" name="message" id="message" rows="8">Type your message</textarea>
                     </div>
-                </div>
+
+                    <button type="submit">
+                        Send
+                    </button>
+                </form>
             </div>
-            <button type="submit">
-                Send
-            </button>
-        </form>
+        </div>
     </section>
 </template>
 
@@ -39,43 +49,79 @@ export default {
 .container {
 
     h1 {
-        padding: 2rem;
+        padding: 0 8rem 3rem;
+        margin-top: -2rem;
+        font-family: "Zilla Slab", serif;
+        font-size: 5rem;
+        color: var(--secondary-dark);
     }
 
-    form {
-        border: 1px solid gray;
-        border-radius: 1rem;
-        padding: 1rem;
+    .row {
+        display: flex;
 
-        .wrapper {
-            display: flex;
+        .contact {
+            width: calc(100% / 12 * 5);
+            padding-left: 6rem;
 
-            .left,
-            .right {
-                width: 50%;
-                padding: 1rem;
+            p {
+                padding: 3rem 6rem;
+                font-size: 2rem;
+                color: var(--primary-dark);
+
+                a {
+                    color: var(--lightest);
+                    font-size: 3rem;
+                    margin-left: 0.5rem;
+                    padding: 0.1rem 0.3rem 0;
+                    line-height: 1.5rem;
+                    transition: 0.4s;
+
+                    &:hover {
+                        color: var(--primary-dark);
+                        background-color: var(--lightest);
+                        border-radius: 0.2rem;
+                    }
+                }
             }
 
-            input,
-            textarea {
-                width: 100%;
-                margin: 1rem 0 2rem;
-                padding: 0.5rem;
-            }
-
-            label {
-                margin-left: 2rem;
+            h2 {
+                font-family: "Zilla Slab", serif;
+                font-size: 3rem;
+                color: var(--secondary-medium);
             }
         }
 
-        button {
-            width: 100%;
-            padding: 1rem 0;
-            font-size: 1.5rem;
+        .form {
+            width: calc(100% / 12 * 7);
 
-            &:hover {
-                cursor: pointer;
-                background-color: lightgray;
+            form {
+                border-image: linear-gradient(var(--primary-dark), var(--primary-light)) 2 / 5px;
+                padding: 2rem 3rem;
+
+
+                input,
+                textarea {
+                    width: 100%;
+                    margin: 1rem 0 2rem;
+                    padding: 0.5rem;
+                    background-color: var(--lightest);
+                }
+
+                label {
+                    margin-left: 2rem;
+                }
+
+                button {
+                    width: 100%;
+                    padding: 1rem 0;
+                    font-size: 1.5rem;
+
+                    &:hover {
+                        cursor: pointer;
+                        background-color: var(--medium);
+                        color: var(--lightest);
+                    }
+                }
             }
         }
     }
